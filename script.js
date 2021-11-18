4c9af37af404cd24a2ad95ecb688199fbb2220a4
Lavish Jain <lavishjain36@gmail.com>
2021-11-18 12:04:40 +0530

    Guessing the Number

--- script.js
+++ script.js
@@ -1,61 +1,20 @@
-//* Get Value from input by button
-document.getElementById("search-btn").addEventListener("click", function () {
-  const searchInputBox = document.getElementById("search-input").value;
-  clickToShow(searchInputBox);
-});
+//Generate a random Number
+let y = Math.floor(Math.random() * 10 + 1);
+console.log(y);
 
-//* Searches Data By fetch API
-function clickToShow(inputValue) {
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => displayMealItem(data));
+let guessCount = 1;
 
-  const displayMealItem = (meals) => {
-    const cardContainer = document.getElementById("card-container");
-    const mealsArray = meals.meals;
+document.getElementById("submitguess").onclick = function () {
+  let x = document.getElementById("GuessField").value;
+  // console.log(x);
 
-    mealsArray.forEach((meal) => {
-      const cardDiv = document.createElement("div");
-      cardDiv.className = "card single-card";
-      const cardInfo = `
-            <img src='${meal.strMealThumb}' class='card-img-top'>
-            <div class='card-body'>
-                <h5 class='card-title text-center'>${meal.strMeal}</h5>
-            </div>
-            <button class='btn btn-success' onclick="singleCardDetail('${meal.idMeal}')">Show Details</button>
-            `;
-      cardDiv.innerHTML = cardInfo;
-      cardContainer.appendChild(cardDiv);
-    });
-  };
-  document.getElementById("search-input").value = "";
-}
-
-//* Search: Enter key trigger.
-document.getElementById("search-input").addEventListener("keyup", (event) => {
-  if (event.key === "Enter") document.getElementById("search-btn").click();
-});
-
-//* Individuals Card Details
-const singleCardDetail = (id) => {
-  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => IndividualPage(data));
-};
-const IndividualPage = (singleData) => {
-  const singleMeal = singleData.meals[0];
-
-  const showSinglePage = document.getElementById("showSinglePage");
-  showSinglePage.innerHTML = `
-        <div class='singleFoodStyle'>
-            <img src='${singleMeal.strMealThumb}'>
-            <ul class='my-auto'>
-                <li><strong>Food of Recipes</strong> : ${singleMeal.strArea}</li>
-                <li><strong>Name of Item</strong> : ${singleMeal.strCategory}</li>
-                <li><strong>Ingredients Item</strong> : ${singleMeal.strIngredient1},${singleMeal.strIngredient2},${singleMeal.strIngredient3},${singleMeal.strIngredient4},${singleMeal.strIngredient4},${singleMeal.strIngredient6},${singleMeal.strIngredient7},${singleMeal.strIngredient8} etc...</li>
-            </ul>
-        </div>
-    `;
+  if (x == y) {
+    alert("COngratulations.You have Guessed Exact Number " + guessCount);
+  } else if (x > y) {
+    alert("Sorry.Try a Smaller Number");
+    guessCount++;
+  } else {
+    alert("Sorry.Try a Bigger Number");
+    guessCount++;
+  }
 };
